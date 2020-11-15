3a2275a30fb1153eac0f1d8e0626d2dcfb42ae6e
sinseman44 <sinseman44@gmail.com>
2020-11-15 07:54:19 +0000

    modify src folder in piplot folder

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description="Control PiPlot2D system",
     long_description_content_type="text/markdown",
     url="https://github.com/sinseman44/PiPlot2D",
-    packages=setuptools.find_packages(where='src'),
+    packages=setuptools.find_packages(where='piplot'),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -27,4 +27,5 @@ setuptools.setup(
         "https://github.com/sinseman44/PyCNC/archive/v2.0.0.zip#egg=pycnc-2.0.0",
     ],
     python_requires='>=3.6',
+    scripts=['piplotter']
 )
